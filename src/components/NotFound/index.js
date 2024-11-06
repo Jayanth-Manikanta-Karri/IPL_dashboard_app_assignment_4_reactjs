d1259a048214f61c7017a804189c6255df5eb6e4
Jayanth-Manikanta-Karri <jayanth.karri2004@gmail.com>
2024-11-06 13:40:51 +0000

    initial commit

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+// Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class NotFound extends Component {
+  render() {
+    return <h1>Page Not Found</h1>
+  }
+}
+
+export default NotFound
